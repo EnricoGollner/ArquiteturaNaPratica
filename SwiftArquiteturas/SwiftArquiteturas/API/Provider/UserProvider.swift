c9324e6117cfaad3a814bdf99d7fbb3fea6121e5
EnricoGollner <enricogollner.tech@gmail.com>
2023-05-27 15:45:51 -0300

    API Provider

---
+++ SwiftArquiteturas/SwiftArquiteturas/API/Provider/UserProvider.swift
@@ -0,0 +1,47 @@
+//
+//  UserProvider.swift
+//  SwiftArquiteturas
+//
+//  Created by Enrico Sousa Gollner on 27/05/23.
+//
+
+import FirebaseAuth
+import Foundation
+
+protocol UserProviderProtocol {
+    func login(parameters: [AnyHashable: Any], completionHandler: @escaping (Result<UserModel, Error>) -> Void)
+    func register(parameters: [AnyHashable: Any], completionHandler: @escaping (Result<UserModel, Error>) -> Void)
+}
+
+class UserProvider: UserProviderProtocol {
+    lazy var auth = Auth.auth()
+    
+    func login(parameters: [AnyHashable : Any], completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
+        let body: NSDictionary = parameters[Constants.ParametersKeys.body] as! NSDictionary
+        let userModel = body[Constants.ParametersKeys.userModel] as! UserModel
+        
+        self.auth.signIn(withEmail: userModel.email, password: userModel.password) { (result, error) in
+            if let error {
+                completionHandler(.failure(error))
+            } else {
+                completionHandler(.success(userModel))
+            }
+        }
+        
+    }
+    
+    func register(parameters: [AnyHashable : Any], completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
+        let body: NSDictionary = parameters[Constants.ParametersKeys.body] as! NSDictionary
+        let userModel = body[Constants.ParametersKeys.userModel] as! UserModel
+        
+        self.auth.createUser(withEmail: userModel.email, password: userModel.password) { (result, error) in
+            if let error {
+                completionHandler(.failure(error))
+            } else {
+                completionHandler(.success(userModel))
+            }
+        }
+    }
+    
+}
+
